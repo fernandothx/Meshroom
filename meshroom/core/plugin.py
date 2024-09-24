0e2626465a0338141c0fef921c9ffc39e096d508
Matthieu Hog <matthieu.hog@mikrosimage.com>
2024-09-24 17:27:58 +0200

    changes for uid renaming

--- meshroom/core/plugin.py
+++ meshroom/core/plugin.py
@@ -298,7 +298,7 @@ def getCommandLine(chunk):
         raise RuntimeError("The project needs to be saved to use plugin nodes")
     saved_graph = loadGraph(chunk.node.graph.filepath)
     if (str(chunk.node) not in [str(f) for f in saved_graph._nodes._objects]
-        or chunk.node._uids[0] != saved_graph.findNode(str(chunk.node))._uids[0] ):
+        or chunk.node._uid != saved_graph.findNode(str(chunk.node))._uid ):
         raise RuntimeError("The changes needs to be saved to use plugin nodes")
 
     cmdPrefix = ""
@@ -342,7 +342,7 @@ def getCommandLine(chunk):
         
     return command
 
-# you may use these to esplicitly define Pluginnodes
+# you may use these to explicitly define Pluginnodes
 class PluginNode(desc.Node):
     pass
 
